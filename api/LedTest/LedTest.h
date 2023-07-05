d34ad7837048983c50074f90866459f198e53082
duan-dky <duan-dky@proton.me>
2023-07-05 19:17:22 +0800

    update LedTest function

--- api/LedTest/LedTest.h
+++ api/LedTest/LedTest.h
@@ -7,6 +7,21 @@
 
 #include <fcntl.h>
 
-void LedTest(int led_no,int status );
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
+
+void sigint_handler(int sig);
+int get_pid_by_name(const char *name);
+int start_process(const char *name);
+int stop_process(const char *name);
+
+bool LedTest(int led_no,int status );
 
 #endif
